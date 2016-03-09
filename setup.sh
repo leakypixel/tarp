de9e65ecd487ef4a061111a9e809bf1a86aaf53a
Craig Fletcher <hi@leakypixel.net>
2016-03-09 12:16:28 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,41 +6,51 @@
 packages="git tmux vim albert conky stalonetray google-chrome-stable nvm npm rvm"
 directories="tmp dev"
 
-function install {
-  if [ $(which apt-get) ]; then
-    sudo apt-get update
-    for package in $1; do
-      sudo apt-get install -y $package
-    done
-  elif [ $(which pacman) ]; then
-    sudo pacman -Syu
-    for package in $1; do
-      sudo pacman -S --noconfirm $1
-    done
-  else
-    echo "Couldn't find package manager :("
-    exit
-  fi
-}
+if [ -n "$packages" ]; then
+  # This needs rewriting, but currently works for ubuntu systems so I'll leave it for now.
+  function install {
+    # deb
+    if [ $(which apt-get) ]; then
+      sudo apt-get update
+      for package in $1; do
+        sudo apt-get install -y $package
+      done
+    # arch
+    elif [ $(which pacman) ]; then
+      sudo pacman -Syu
+      for package in $1; do
+        sudo pacman -S --noconfirm $package
+      done
+    else
+    # nobody uses fedora :trollface:
+      echo "Couldn't find package manager :("
+      exit
+    fi
+  }
 
-for package in $packages; do
-  if [ $(which $package) ]; then
-    echo $package already installed.
-  else
-    packages_to_install+=" $package"
-  fi
-done
+  for package in $packages; do
+    if [ $(which $package) ]; then
+      echo $package already installed.
+    else
+      packages_to_install+=" $package"
+    fi
+  done
 
-if [ $packages_to_install ]; then 
-  echo Installing: $packages_to_install
-  install $packages_to_install
+  if [ -n "$packages_to_install" ]; then
+    echo Installing: $packages_to_install
+    for package in $packages_to_install; do
+      install $package
+    done
+  fi
 fi
 
 cd
 
-for dir in $directories; do
-  mkdir -p $dir
-done
+if [ -n "$directories" ]; then
+  for dir in $directories; do
+    mkdir -p $dir
+  done
+fi
 
 
 git clone https://github.com/leakypixel/tarp.git

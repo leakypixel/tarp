86b5cb555c6422045ae38c2ae937dbc3efd7f5c1
Craig Fletcher <hi@leakypixel.net>
2016-03-17 14:01:01 +0000

    Auto backup

---
+++ home/.bash/git-completion.sh
@@ -0,0 +1,20 @@
+# Git completion
+
+function set_git_completion {
+  # Add git completion to my custom 'g' alias.
+  __git_complete g __git_main
+}
+
+if [ -f /usr/share/git/completion/git-completion.bash ]; then
+  source /usr/share/git/completion/git-completion.bash
+  set_git_completion
+else
+  sudo mkdir -p /usr/share/git/completion
+  sudo wget -O /usr/share/git/completion/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  if [ -f /usr/share/git/completion/git-completion.bash ]; then
+    source /usr/share/git/completion/git-completion.bash
+    set_git_completion
+  else
+    echo "Could not load git completion :("
+  fi
+fi

5cce967cb8bb1253e6e0f38e6e4ca2ddd80d67a9
Craig Fletcher <hi@leakypixel.net>
2016-04-21 17:50:24 +0100

    Auto backup

--- home/.dashboard/start-dashboard.sh
+++ home/.dashboard/start-dashboard.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+touch /home/craigf/great_success
 conky -c $HOME/.dashboard/.conkystatsrc &
 conky -c $HOME/.dashboard/.conkytimerc &
 stalonetray -c $HOME/.dashboard/.stalonetrayrc &

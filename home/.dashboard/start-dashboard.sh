cc4eb0e3ff79576bf6652f0c12ae559339a6acfe
Craig Fletcher <hi@leakypixel.net>
2016-12-06 18:25:15 +0000

    Delete old configs

--- home/.dashboard/start-dashboard.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sleep 5;
-feh --bg-scale $HOME/.dashboard/wallpaper.jpg &
-conky -c $HOME/.dashboard/.conkystatsrc &
-conky -c $HOME/.dashboard/.conkytimerc &
-stalonetray -c $HOME/.dashboard/.stalonetrayrc &

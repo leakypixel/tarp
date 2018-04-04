b813a2f0f0ae05a7084bac380d1801098f61e070
Craig Fletcher <hi@leakypixel.net>
2018-04-04 16:20:46 +0100

    Remove old scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,14 +3,12 @@
 basedir=$HOME/tarp
 cd $basedir
 
-$basedir/scripts/install-system-packages.sh
 $basedir/scripts/create-directories.sh
 
 git submodule init
 git submodule up
 
 $basedir/scripts/update-links.sh
-$basedir/scripts/install-node-packages.sh
 
 branch_exists=$(git branch -a | grep -l `hostname`)
 if [ -z "$branch_exists" ]; then

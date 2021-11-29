13d0baaadbd838c4222a619d72efd12a385b4685
Craig Fletcher <hi@leakypixel.net>
2021-11-29 15:47:57 +0000

    Auto backup

--- home/.bash/50-ruby.sh
+++ home/.bash/50-ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # RVM/Ruby
-#export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH="$PATH:$HOME/.gem/ruby/2.6.0/bin"
-#[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
+export PATH="$PATH:$HOME/.gem/ruby/3.0.0/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*

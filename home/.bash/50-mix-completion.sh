97c09042647cf3d376664a6598a7f44c5a1c33f9
Craig Fletcher <hi@leakypixel.net>
2021-12-02 16:07:46 +0000

    Auto backup

--- home/.bash/50-mix-completion.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-_mix_completions()
-{
-  COMPREPLY=($(compgen -W "$(mix help | sed 's/^mix //; s/ .*$//g; s/^iex$//' | paste -sd " " -)" "${COMP_WORDS[1]}"))
-}
-
-complete -F _mix_completions mix
